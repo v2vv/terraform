bfc1a8edb4c72e168430094b20252a3920d0d44e
haitianlan <31479870+v2vv@users.noreply.github.com>
2024-06-04 00:44:29 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
