89047b833dda9c6338bef7fff9ee26377a5838f4
haitianlan <31479870+v2vv@users.noreply.github.com>
2024-06-04 00:36:55 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+
 provider "null" {}
 
 resource "null_resource" "install_docker" {
