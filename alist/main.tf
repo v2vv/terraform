847484e6a37b270670d1593418b004d475ae9795
haitianlan <31479870+v2vv@users.noreply.github.com>
2024-06-04 11:24:38 +0800

    升级 alist

--- alist/main.tf
+++ alist/main.tf
@@ -44,7 +44,9 @@ resource "null_resource" "alist" {
 
   provisioner "remote-exec" {
     inline = [
-      "docker run -d --restart=always -v /media/U盘:/media/U盘 -v /root/alist:/opt/alist/data -p 5244:5244 -e PUID=0 -e PGID=0 -e UMASK=022 --name="alist" xhofe/alist:latest"
+      <<EOF
+      docker run -d --restart=always -v /media/U盘:/media/U盘 -v /root/alist:/opt/alist/data -p 5244:5244 -e PUID=0 -e PGID=0 -e UMASK=022 --name="alist" xhofe/alist:latest
+      EOF
     ]
     connection {
       type        = "ssh"
