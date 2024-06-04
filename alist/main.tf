1b043b7c50ae025926435668a471a576430fa85d
haitianlan <31479870+v2vv@users.noreply.github.com>
2024-06-04 11:18:15 +0800

    升级 alist

--- alist/main.tf
+++ alist/main.tf
@@ -44,9 +44,7 @@ resource "null_resource" "alist" {
 
   provisioner "remote-exec" {
     inline = [
-      "docker",
-      "pwd",
-      "ls"
+      "docker run -d --restart=always -v /media/U盘:/media/U盘 -v /root/alist:/opt/alist/data -p 5244:5244 -e PUID=0 -e PGID=0 -e UMASK=022 --name="alist" xhofe/alist:latest",
     ]
 
     connection {
